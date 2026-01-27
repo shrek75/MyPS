4428eba5fd05100e1bb23b4abdce46fe67e17e32
Shrek슈렉 <freshine0@gmail.com>
2026-01-27 11:34:21 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int input;
+    cin >> input;
+    if (input)
+        cout << "Leading the Way to the Future";
+    else
+        cout << "YONSEI";
+
+
+    return 0;
+}
