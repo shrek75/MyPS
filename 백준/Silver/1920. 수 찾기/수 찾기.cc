e041f57db7b432ff496eadcab12e59bbb8dc9356
Shrek슈렉 <freshine0@gmail.com>
2026-03-19 09:55:03 +0900

    [Silver IV] Title: 수 찾기, Time: 76 ms, Memory: 6820 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <unordered_set>
+
+using namespace std;
+
+
+int main()
+{
+	//bool arr[UINT_MAX];
+	//
+	//for (unsigned int i = 0; i <= UINT_MAX; i++)
+	//{
+	//	arr[i] = 1;
+	//}
+
+	//bool* arr;
+	//arr = new bool[UINT_MAX+1];
+	//
+	//for (long long int i = 0; i <= UINT_MAX; i++)
+	//{
+	//	arr[i] = 1;
+	//}
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	unordered_set<int> set;
+	int n, m, x;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> x;
+		set.insert(x);
+	}
+	cin >> m;
+	for (int i = 0; i < m; i++)
+	{
+		cin >> x;
+		cout << set.count(x) << '\n';
+	}
+
+	return 0;
+}
